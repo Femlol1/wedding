986dda6f1ebf4ed37017fd78084a8c9cc74c39d9
Femi <osibemekunfemi@gmail.com>
2024-07-28 20:49:43 +0100

    remove env

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -1,8 +1,12 @@
 
 const gallery = () => {
   return (
-    <div>gallery
-    
+    <div className="container mx-auto mt-20">
+    <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+    <div className="wrapper h3-bold text-center">RSVP</div>
+    </section>
+    <div className="wrapper my-8">
+    </div>
     </div>
   )
 }
