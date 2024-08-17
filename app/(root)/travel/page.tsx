abbc6217903456e23a636b5176e9ca7633e32b28
Femi <osibemekunfemi@gmail.com>
2024-08-17 15:35:40 +0100

    thing to do in lagos

--- app/(root)/travel/page.tsx
+++ app/(root)/travel/page.tsx
@@ -1,4 +1,4 @@
-// pages/travel.js
+// pages/travel.tsx
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -39,32 +39,90 @@ export default function Travel() {
                             <div className="md:w-2/3">
                                 <h3 className="text-xl font-semibold">Hotel Vintanio</h3>
                                 <p>We have reserved Hotel Vintanio in Lekki for those seeking accommodation at N80,000 per night. Please contact the hotel manager at <span className='text-primary-500 hover:text-primary-700'><a href={`tel:${`+234813508382`}`}>+234(0)813508382</a></span> to book a room at our discounted rate.</p>
-                                <p>More information: <Link href="https://vintanohotel.com/" legacyBehavior><a className="text-primary-500 hover:text-primary-700">Hotel Vintanio Website</a></Link></p>
-                            </div>\
-                        </div>
-                        
-                        {/* <div className="mt-4 flex flex-col md:flex-row items-center md:items-start gap-4">
-                            <Image
-                                src="/assets/images/hotel-mainland.jpg" // Replace with the actual image path
-                                alt="Hotel Mainland"
-                                width={300}
-                                height={200}
-                                className="rounded-lg w-full md:w-1/3 object-cover"
-                            />
-                            <div className="md:w-2/3">
-                                <h3 className="text-xl font-semibold">Mainland - Hotel [Hotel Name]</h3>
-                                <p>Description and details about the mainland hotel accommodation.</p>
-                                <p>Contact [phone number] for bookings.</p>
+                                <p>More information: <Link className="text-primary-500 hover:text-primary-700 underline" href="https://vintanohotel.com/"> Hotel Vintanio Website </Link></p>
                             </div>
-                        </div> */}
+                        </div>
                     </section>
 
                     <section>
                         <h2 className="text-2xl font-semibold">Airbnb Options</h2>
-                        <p className="mt-2">There are also numerous Airbnb options available around the event locations. Here are links to Airbnb listings recommended for their quality and proximity to our wedding events:</p>
-                        <ul className="list-disc list-inside">
-                            <li><Link href="https://www.airbnb.com/s/Lagos--Nigeria/homes" legacyBehavior><a className="text-primary-500 hover:text-primary-700">Airbnbs near Lagos</a></Link></li>
+                        <p className="mt-2">There are also numerous Airbnb options available around the event locations. Here is a link to Airbnb listings recommended for their quality and proximity to our wedding events: <span><Link className='text-primary-500 hover:text-primary-700 underline' href="https://www.airbnb.com/s/Lagos--Nigeria/homes" >Airbnbs near Lagos</Link></span></p>
+                    </section>
+                    
+                    <section>
+                        <h2 className="text-2xl font-semibold mt-6">Things to do in Lagos</h2>
+                        <p className="mt-2">Lagos is a vibrant city with plenty to offer. Whether you are interested in cultural sites, shopping, or outdoor activities, there&apos;s something for everyone. Below are some recommendations:</p>
+                        <ul className="mt-4 list-disc list-inside space-y-2">
+                            <li>
+                                <Link className='text-primary-500 hover:text-primary-700 underline' href="https://www.tripadvisor.co.uk/Attractions-g304026-Activities-Lagos_Lagos_State.html" target="_blank">
+                                    Explore Top Attractions on TripAdvisor
+                                </Link> – Get a list of the best things to do in Lagos, from cultural sites to beaches.
+                            </li>
+                            <li>
+                                <Link className='text-primary-500 hover:text-primary-700 underline' href="https://www.viator.com/Nigeria/d22316-ttd" target="_blank">
+                                    Explore Tours and Activities on Viator
+                                </Link> – Discover exciting tours, activities, and attractions in Lagos and beyond.
+                            </li>
+                            <li>
+                                <div className="mt-4 flex flex-col md:flex-row items-center md:items-start gap-4">
+                                    <Image
+                                        src="/assets/images/Travel/lekki-market.jpg" // Replace with the actual image path
+                                        alt="Lekki Market"
+                                        width={300}
+                                        height={200}
+                                        className="rounded-lg w-full md:w-1/3 object-cover"
+                                    />
+                                    <p className="text-gray-700">
+                                        Enjoy the bustling markets like Lekki Market or Balogun Market where you can buy local crafts and souvenirs.
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="mt-4 flex flex-col md:flex-row items-center md:items-start gap-4">
+                                    <Image
+                                        src="/assets/images/Travel/nike-art-gallery.jpg" // Replace with the actual image path
+                                        alt="Nike Art Gallery"
+                                        width={300}
+                                        height={200}
+                                        className="rounded-lg w-full md:w-1/3 object-cover"
+                                    />
+                                    <p className="text-gray-700">
+                                        Visit the Nike Art Gallery, one of the largest art galleries in West Africa, showcasing the best of Nigerian art.
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="mt-4 flex flex-col md:flex-row items-center md:items-start gap-4">
+                                    <Image
+                                        src="/assets/images/Travel/Tarkwa-Bay-Beach.jpg" // Replace with the actual image path
+                                        alt="Tarkwa Bay Beach"
+                                        width={300}
+                                        height={200}
+                                        className="rounded-lg w-full md:w-1/3 object-cover"
+                                    />
+                                    <p className="text-gray-700">
+                                        Spend a day at Tarkwa Bay Beach, an ideal spot for relaxation and water sports.
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="mt-4 flex flex-col md:flex-row items-center md:items-start gap-4">
+                                    <Image
+                                        src="/assets/images/Travel/Lagos-National-Museum.jpeg" // Replace with the actual image path
+                                        alt="Lagos National Museum"
+                                        width={300}
+                                        height={200}
+                                        className="rounded-lg w-full md:w-1/3 object-cover"
+                                    />
+                                    <p className="text-gray-700">
+                                        Discover the history of Lagos at the National Museum, where you can learn about Nigeria’s rich cultural heritage.
+                                    </p>
+                                </div>
+                            </li>
                         </ul>
+                        <p className="mt-4 text-gray-700">
+                            For more personalized recommendations and assistance with bookings, feel free to reach out to our wedding planner at <span className='text-primary-500 hover:text-primary-700'><a href={`tel:${`+234813508382`}`}>+234(0)813508382</a></span>.
+                        </p>
                     </section>
 
                     <section className="fixed bottom-4 right-4 z-20">
