8dd10c9e922ca198adb232aca999662614faa139
Femi <osibemekunfemi@gmail.com>
2025-02-03 19:36:05 +0000

    Update NavItems component to include '/create-rsvp' path in admin link determination

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,39 +1,48 @@
-'use client';
+"use client";
 
 import { adminLinks, userLinks } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { FC } from 'react';
+import { FC } from "react";
 
 interface NavItemsProps {
-    handleClose: () => void; // Define the type for the handleClose prop
+	handleClose: () => void; // Define the type for the handleClose prop
 }
 
 const NavItems: FC<NavItemsProps> = ({ handleClose }) => {
-    const pathname = usePathname();
+	const pathname = usePathname();
 
-    // Determine if the current path is for admin or check-in
-    const isAdminPath = pathname.startsWith('/admin') || pathname.startsWith('/check-in') || pathname.startsWith('/stats')|| pathname.startsWith('/comments');
+	// Determine if the current path is for admin or check-in
+	const isAdminPath =
+		pathname.startsWith("/admin") ||
+		pathname.startsWith("/check-in") ||
+		pathname.startsWith("/stats") ||
+		pathname.startsWith("/comments") ||
+		pathname.startsWith("/create-rsvp");
 
-    // Choose the appropriate links based on the current path
-    const links = isAdminPath ? adminLinks : userLinks;
+	// Choose the appropriate links based on the current path
+	const links = isAdminPath ? adminLinks : userLinks;
 
-    return (
-        <ul className="md:flex-between flex w-full flex-full flex-col items-start gap-5 md:flex-row">
-            {links.map((link) => {
-                const isActive = pathname === link.route;
+	return (
+		<ul className="md:flex-between flex w-full flex-full flex-col items-start gap-5 md:flex-row">
+			{links.map((link) => {
+				const isActive = pathname === link.route;
 
-                return (
-                    <li
-                        key={link.route}
-                        className={`${isActive ? 'text-primary-500' : ''} flex-center p-medium-16 whitespace-nowrap`}
-                    >
-                        <Link href={link.route} onClick={handleClose}>{link.label}</Link>
-                    </li>
-                );
-            })}
-        </ul>
-    );
-}
+				return (
+					<li
+						key={link.route}
+						className={`${
+							isActive ? "text-primary-500" : ""
+						} flex-center p-medium-16 whitespace-nowrap`}
+					>
+						<Link href={link.route} onClick={handleClose}>
+							{link.label}
+						</Link>
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
 
 export default NavItems;
